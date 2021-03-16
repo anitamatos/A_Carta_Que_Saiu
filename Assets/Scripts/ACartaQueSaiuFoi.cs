0231c30cb777fb33af4230921e5b38405a590b99
anitamatos <40200233@esmad.ipp.pt>
2021-03-16 22:21:38 +0000

    versao1

---
+++ Assets/Scripts/ACartaQueSaiuFoi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACartaQueSaiuFoi : MonoBehaviour
+{
+    string[] figuras;
+    string[] pintas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        figuras = new[] { "Ás", "Rei", "Dama", "Valete", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
+        pintas = new[] { "Copas", "Espadas", "Ouros", "Paus" };
+        Debug.Log("Olá, se queres uma carta do meu baralho, então carrega no espaço");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int figura = Random.Range(0, 12);
+            int pinta = Random.Range(0, 3);
+
+            Debug.Log("Saiu a carta " + figuras[figura] + " " + pintas[pinta]);
+        }
+    }
+}
